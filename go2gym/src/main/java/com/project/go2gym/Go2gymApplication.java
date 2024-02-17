bdd7edbaa855f865a6d0f5d15c80ce38e252dac5
Leonardo Mormontoy <lmormontoyc@gmail.com>
2024-02-17 12:40:32 -0800

    start

---
+++ go2gym/src/main/java/com/project/go2gym/Go2gymApplication.java
@@ -0,0 +1,13 @@
+package com.project.go2gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Go2gymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Go2gymApplication.class, args);
+	}
+
+}
---
+++ go2gym/src/test/java/com/project/go2gym/Go2gymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.go2gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Go2gymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
